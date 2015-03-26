ec544adedc1709c06f3f95191b3723dff26d958d
Cecile Robert-Michon <cecile.robert-michon@mail.mcgill.ca>
2015-03-26 15:20:22 -0400

    format

--- src/OdometryCorrection.java
+++ src/OdometryCorrection.java
@@ -12,73 +12,73 @@ import lejos.nxt.Sound;
 
 public class OdometryCorrection extends Thread {
 
- private Odometer odometer;
- private Robot robot;
- private double xLS, yLS, ex, ey;
+	private Odometer odometer;
+	private Robot robot;
+	private double xLS, yLS, ex, ey;
 
- // constructor
- public OdometryCorrection(Odometer odometer) {
-  this.odometer = odometer;
-  this.robot = new Robot();
- }
+	// constructor
+	public OdometryCorrection(Odometer odometer) {
+		this.odometer = odometer;
+		this.robot = new Robot();
+	}
 
- // run method (required for Thread)
- public void run() {
-  long correctionStart, correctionEnd;
-  int previousLight = robot.COLOR_SENSOR.getNormalizedLightValue();
+	// run method (required for Thread)
+	public void run() {
+		long correctionStart, correctionEnd;
+		int previousLight = robot.COLOR_SENSOR.getNormalizedLightValue();
 
-  while (true) {
-   correctionStart = System.currentTimeMillis();
+		while (true) {
+			correctionStart = System.currentTimeMillis();
 
-   // put your correction code here
-   int light = robot.COLOR_SENSOR.getNormalizedLightValue();
+			// put your correction code here
+			int light = robot.COLOR_SENSOR.getNormalizedLightValue();
 
-   // LCD.drawString("Light : " + light, 0, 5);
-   if (previousLight - light > robot.LIGHTSENSOR_THRESHOLD) {
+			// LCD.drawString("Light : " + light, 0, 5);
+			if (previousLight - light > robot.LIGHTSENSOR_THRESHOLD) {
 
-    xLS = odometer.getX()
-      + (-robot.LIGHT_SENSOR_DISTANCE * Math.sin(Math
-        .toRadians(odometer.getTheta())));
-    yLS = odometer.getY()
-      + (-robot.LIGHT_SENSOR_DISTANCE * Math.cos(Math
-        .toRadians(odometer.getTheta())));
+				xLS = odometer.getX()
+						+ (-robot.LIGHT_SENSOR_DISTANCE * Math.sin(Math
+								.toRadians(odometer.getTheta())));
+				yLS = odometer.getY()
+						+ (-robot.LIGHT_SENSOR_DISTANCE * Math.cos(Math
+								.toRadians(odometer.getTheta())));
 
-    if (xLS % robot.TILE_LENGTH < robot.HALF_TILE) {
-     ex = xLS % robot.TILE_LENGTH;
-    } else {
-     ex = (xLS % robot.TILE_LENGTH) - robot.TILE_LENGTH;
-    }
-    if (yLS % robot.TILE_LENGTH < robot.HALF_TILE) {
-     ey = yLS % robot.TILE_LENGTH;
-    } else {
-     ey = (yLS % robot.TILE_LENGTH) - robot.TILE_LENGTH;
-    }
+				if (xLS % robot.TILE_LENGTH < robot.HALF_TILE) {
+					ex = xLS % robot.TILE_LENGTH;
+				} else {
+					ex = (xLS % robot.TILE_LENGTH) - robot.TILE_LENGTH;
+				}
+				if (yLS % robot.TILE_LENGTH < robot.HALF_TILE) {
+					ey = yLS % robot.TILE_LENGTH;
+				} else {
+					ey = (yLS % robot.TILE_LENGTH) - robot.TILE_LENGTH;
+				}
 
-    //if ((Math.min(Ex, Ey) / Math.max(Ex, Ey) < 0.8) && Math.sqrt(Ex*Ex + Ey*Ey) > 2.0) {
-     if (ex < ey) {
-      odometer.setX(odometer.getX() - ex);
-     } else {
-      odometer.setY(odometer.getY() - ey);
-     }
-     
-    // this ensure the odometry correction occurs only once every
-    // period
+				// if ((Math.min(Ex, Ey) / Math.max(Ex, Ey) < 0.8) &&
+				// Math.sqrt(Ex*Ex + Ey*Ey) > 2.0) {
+				if (ex < ey) {
+					odometer.setX(odometer.getX() - ex);
+				} else {
+					odometer.setY(odometer.getY() - ey);
+				}
 
-    // previousLight = light;
-   }
-      correctionEnd = System.currentTimeMillis();
-    if (correctionEnd - correctionStart < robot.CORRECTION_PERIOD) {
-     try {
-      Thread.sleep(robot.CORRECTION_PERIOD
-        - (correctionEnd - correctionStart));
-     } catch (InterruptedException e) {
-      // there is nothing to be done here because it is not
-      // expected that the odometry correction will be
-      // interrupted by another thread
-     }
-    }
-  
-  }
- }
+				// this ensure the odometry correction occurs only once every
+				// period
+
+			}
+			correctionEnd = System.currentTimeMillis();
+			if (correctionEnd - correctionStart < robot.CORRECTION_PERIOD) {
+				try {
+					Thread.sleep(robot.CORRECTION_PERIOD
+							- (correctionEnd - correctionStart));
+				} catch (InterruptedException e) {
+					// there is nothing to be done here because it is not
+					// expected that the odometry correction will be
+					// interrupted by another thread
+				}
+			}
+
+		}
+	}
 
 }
